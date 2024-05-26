8325538a53ae05523b1355b068645fc47055d94f
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-05-26 14:16:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let yourArray = [
+  "Hello",     // string
+  42,          // number
+  true,        // boolean
+  { name: "Alice" }, // object
+  [1, 2, 3],   // array
+];
+
+console.log(yourArray.length); // logs 5
 
